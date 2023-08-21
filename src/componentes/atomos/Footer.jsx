560c2a6a7a497d685be6255d23c2158ad476c716
221236CarlosCruz <carlos.programacion@gruporyc.com.mx>
2023-08-21 16:17:16 -0600

    archivos corregidos

--- src/componentes/atomos/Footer.jsx
+++ src/componentes/atomos/Footer.jsx
@@ -1,88 +1,169 @@
+import React, { useState, useRef } from "react";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { useRef } from "react";
 import "../../assets/styles/Footer.css";
 import IconoPoots from "../../assets/img/IconoPoots.png";
 import { GoLocation } from "react-icons/go";
 import { MdCall } from "react-icons/md";
 import { BiLogoInstagramAlt } from "react-icons/bi";
+import Swal from "sweetalert2";
 import { PiTiktokLogoFill } from "react-icons/pi";
 import { BiLogoFacebookCircle } from "react-icons/bi";
+import RedesSociales from "./RedesSociales";
+
+
 
 function Footer() {
+
+  const [formData, setFormData] = useState({
+    name: "",
+    telefono: "",
+    correo: ""
+  });
+  
   const formDataF = useRef();
+
   const handlerClick = (e) => {
     e.preventDefault();
     const formData = new FormData(formDataF.current);
-    let URI = "http://localhost:80/Users";
-    let options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        Nombre: formData.get("name"),
-        Telefono: formData.get("telefono"),
-        Correo: formData.get("correo"),
-      }),
-    };
-    fetch(URI, options).then((response) => response.json());
-    Swal.fire({
-      position: "top-end",
-      width: 200,
-      icon: "success",
-      title: "Usuario Registrado",
-      text: JSON.stringify("Gracias"),
-      showConfirmButton: false,
-      timer: 1500,
-    });
+    const userEmail = formData.get("correo");
+
+    fetch("http://52.70.189.119:3000/Users")
+      .then((response) => response.json())
+      .then((correosExistentes) => {
+        const listaCorreos = correosExistentes.map((usuario) => usuario.Correo);
+
+        if (listaCorreos.includes(userEmail)) {
+          Swal.fire({
+            icon: "error",
+            title: "Error al Registrarse",
+            text: `El correo ${userEmail} ya está en uso.`,
+            width: "960"
+          });
+        } else {
+          let URI = "http://52.70.189.119:3000/Users";
+          let options = {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+              Nombre: formData.get("name"),
+              Telefono: formData.get("telefono"),
+              Correo: userEmail
+            })
+          };
+
+          fetch(URI, options)
+            .then((response) => response.json())
+            .then((data) => {
+              if (data.message === "Usuario registrado exitosamente") {
+                setFormData({
+                  name: "",
+                  telefono: "",
+                  correo: ""
+                });
+
+                Swal.fire({
+                  position: "top-center",
+                  height: 600,
+                  width: 614,
+                  padding: 100,
+                  imageUrl:
+                    "https://www.freeiconspng.com/thumbs/success-icon/success-icon-10.png",
+                  imageWidth: "200",
+                  imageHeight: "200",
+                  color: "#4B4124",
+                  title: "¡Registro exitoso!",
+                  text: JSON.stringify("Bienvenido"),
+                  confirmButtonText: "Aceptar",
+                  confirmButtonColor: "#568943"
+                });
+              } else {
+                Swal.fire({
+                  icon: "error",
+                  title: "Error al registrar usuario",
+                  text: data.error
+                });
+              }
+            })
+            .catch((error) => {
+              console.error("Error:", error);
+            });
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   };
 
   return (
     <div className="fondo-footer">
-      <div class="d-flex justify-content-around">
-        <div className="formulario">
+      {/*<div class="d-flex justify-content-around"> */}
+      <div className="d-flex flex-wrap justify-content-center">
+        <div className="formulario column">
           <div className="p-2">
-            <Form ref={formDataF}>
+             <Form ref={formDataF}>
               <Form.Group className="mb-3" controlId="formGroupEmail">
                 <Form.Label className="txt-formularios-nom">Nombre:</Form.Label>
-                <Form.Control type="text" id="name" name="name" />
+                <Form.Control
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={formData.name}
+                  onChange={(e) =>
+                    setFormData({ ...formData, name: e.target.value })
+                  }
+                />
               </Form.Group>
-
               <Form.Group className="mb-3" controlId="formGroupPassword">
-                <Form.Label className="txt-formularios-nom">
-                  Teléfono:
-                </Form.Label>
-                <Form.Control type="number" id="telefono" name="telefono" />
+                <Form.Label className="txt-formularios-nom">Teléfono:</Form.Label>
+                <Form.Control
+                  type="number"
+                  id="telefono"
+                  name="telefono"
+                  value={formData.telefono}
+                  onChange={(e) =>
+                    setFormData({ ...formData, telefono: e.target.value })
+                  }
+                />
               </Form.Group>
-
               <Form.Group className="mb-3" controlId="formGroupPassword">
                 <Form.Label className="txt-formularios-nom">Correo:</Form.Label>
-                <Form.Control type="text" id="correo" name="correo" />
+                <Form.Control
+                  type="text"
+                  id="correo"
+                  name="correo"
+                  value={formData.correo}
+                  onChange={(e) =>
+                    setFormData({ ...formData, correo: e.target.value })
+                  }
+                />
               </Form.Group>
-
               <Form.Group className="mb-3" controlId="formGroupPassword">
                 <button
                   variant="primary"
                   size="lg"
-                  className="button-formulario"
+                  className="col button-formulario"
                   onClick={handlerClick}
                 >
-                  <img src={IconoPoots} />
-                  enviar
+                  <img src={IconoPoots} alt="Icono" />
+                  Enviar
                 </button>
               </Form.Group>
             </Form>
           </div>
         </div>
 
-        <div className="contacto">
+        <div className="contacto column">
           <div className="p-2">
             <div className="TC">
-              <span>contacto</span>
+              <span>
+                <MdCall/> contacto
+              </span>
             </div>
             <Container>
               <div className="num1">
@@ -105,78 +186,40 @@ function Footer() {
             </Container>
           </div>
         </div>
-        <div className="redes-sociales">
+
+        <div className="direccion column">
           <div className="p-2">
-            <div className="rs">
-              <span className="txt-name-secciones">redes sociales</span>
+            <div className="dn">
+              <span>
+                <GoLocation /> direccion
+              </span>
             </div>
             <Container>
-              <div>
+              <div className="dire">
                 <Row>
-                  <Col className="txt-numerosRedes1">
-                    <BiLogoInstagramAlt /> @Poot´sPorLaBuenaVida
-                  </Col>
-                </Row>
-              </div>
-              <div>
-                <Row>
-                  <Col className="txt-numerosRedes">
-                    <PiTiktokLogoFill /> @Poot´sPorLaBuenaVida
-                  </Col>
-                </Row>
-              </div>
-              <div>
-                <Row>
-                  <Col className="txt-numerosRedes">
-                    {" "}
-                    <BiLogoFacebookCircle /> @Poot´sPorLaBuenaVida
-                  </Col>
+                  <Col>2a Norte #26 entre 5a y 6a<br/> 
+                  Oriente Col. Centro</Col>
                 </Row>
               </div>
             </Container>
-            <div className="direccion">
-              <div className="p-2">
-                <div className="dn">
-                  <span>direccion</span>
-                </div>
-                <Container>
-                  <div className="dire">
-                    <Row>
-                      <Col>
-                        2a norte #26 entre quinta y 6a oriente Col. Centro
-                      </Col>
-                    </Row>
-                  </div>
-                </Container>
-              </div>
-            </div>
-
-            <div className="redes-sociales">
-              <div className="p-2">
-                <div className="rs">
-                  <span>redes sociales</span>
-                </div>
-                <Container>
-                  <div className="rsi">
-                    <Row>
-                      <Col>@Poot´sPorLaBuenaVida</Col>
-                    </Row>
-                  </div>
-                  <div className="rst">
-                    <Row>
-                      <Col>@Poot´sPorLaBuenaVida</Col>
-                    </Row>
-                  </div>
-                  <div className="rsf">
-                    <Row>
-                      <Col>@Poot´sPorLaBuenaVida</Col>
-                    </Row>
-                  </div>
-                </Container>
-              </div>
-            </div>
           </div>
         </div>
+
+        <div className="redes-sociales column">
+          <div className="p-2">
+            <div className="rs">
+              <span></span>
+            </div>
+            <Container>
+              <RedesSociales></RedesSociales>
+            </Container>
+
+            <div><span></span></div>
+
+          </div>
+        </div>
+
+
       </div>
     </div>
   );
