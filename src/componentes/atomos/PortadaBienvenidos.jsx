fc4c035b5ecebafc977986dd2ad1c4cc797aa05e
221236CarlosCruz <carlos.programacion@gruporyc.com.mx>
2023-08-21 17:43:08 -0600

    cambios

--- src/componentes/atomos/PortadaBienvenidos.jsx
+++ src/componentes/atomos/PortadaBienvenidos.jsx
@@ -1,13 +1,15 @@
 import IconoPoots from "../../assets/img/IconoPoots.png";
+import LogoPootsBlanco from "../../assets/img/pootsBlanco.png";
 import "../../assets/styles/PortadaBienvenidos.css";
 
 function PortadaBienvenidos() {
   return (
     <div className="background-image-container">
       <div className="overlay">
-        <h1>Logo en blanco</h1>
+        {/*         <h1>Logo en blanco</h1> */}
+        <img src={LogoPootsBlanco} />
         <button className="btn-bienvenidos">
-          <img src={IconoPoots}/>
+          <img src={IconoPoots} />
           explorar
         </button>
       </div>
